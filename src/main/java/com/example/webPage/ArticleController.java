8aaf10b5b139623367ca628b98dd426bf115a25f
NarooSister <dongeun49@naver.com>
2024-01-09 13:43:31 +0900

    게시글 등록화면(write), 게시글 상세화면 구현

--- src/main/java/com/example/webPage/ArticleController.java
+++ src/main/java/com/example/webPage/ArticleController.java
@@ -3,8 +3,7 @@ package com.example.webPage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("article")
@@ -20,6 +19,35 @@ public class ArticleController {
     @GetMapping("write")
     public String articleWrite(){
         return "article/write";
+    }
+    //CREATE
+    //http post요청이 /article 경로로 들어왔을 때 이 메서드가 처리
+    @PostMapping
+    public String articleCreate(
+            @RequestParam("title")
+            String title,
+            @RequestParam("content")
+            String content,
+            @RequestParam("password")
+            String password
+    ){
+        //받아온 데이터를 이용하여 ArticleDto을 생성하고
+        // articleService.create 메서드를 이용해
+        //실제로 새로운 게시글로 저장.
+        //이렇게 생성된 게시글의 새 Id를 newId에 저장
+        //성공적으로 생성되면 article/newId로 리다이렉트
+        Long newId = articleService.create(new ArticleDto(title, content, password)).getId();
+            return String.format("redirect:/article/%d", newId);
+    }
+
+    @GetMapping("{id}")
+    public String articleOne(
+            @PathVariable("id")
+            Long id,
+            Model model
+    ){
+        model.addAttribute("article", articleService.read(id));
+        return "article/read";
     }
 
 }
--- src/main/java/com/example/webPage/ArticleService.java
+++ src/main/java/com/example/webPage/ArticleService.java
@@ -1,6 +1,7 @@
 package com.example.webPage;
 
 import lombok.RequiredArgsConstructor;
+import org.hibernate.dialect.function.array.ArrayToStringFunction;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,5 +26,22 @@ public class ArticleService {
             articlelist.add(ArticleDto.fromEntity(article));
         }
         return articlelist;
+    }
+
+    public ArticleDto read(Long id){
+        Article article = articleRepository.findById(id).orElseThrow();
+                return ArticleDto.fromEntity(article);
+    }
+
+    //수정할 때, 비밀번호는 수정 불가함
+    public ArticleDto update(Long id, ArticleDto dto){
+        Article article = articleRepository.findById(id).orElseThrow();
+        article.setTitle(dto.getTitle());
+        article.setContent(dto.getContent());
+        return ArticleDto.fromEntity(articleRepository.save(article));
+    }
+
+    public void delete(Long id){
+        articleRepository.delete(articleRepository.findById(id).orElseThrow());
     }
 }
